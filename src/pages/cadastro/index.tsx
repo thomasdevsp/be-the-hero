ff6587508390c43538b3351d58bf2d415bff989c
Thomasdevsp <thomasdevsp@gmail.com>
2025-05-05 10:30:21 -0300

    test

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -16,6 +16,7 @@ export function Cadastro() {
 
           <TextContainer>
             <h1>Cadastro</h1>
+
             <p>
               Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da
               sua ONG
